4136d094e459c3ecd44743e4db30e2c51d85cabe
Farai Muzanenhamo <fmuzah@gmail.com>
2025-02-26 15:01:12 +0200

    author

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,7 +1,9 @@
-// Predict and explain first...
+// Predict and explain first... 
+We will get an error due to the loop using the for...of synax.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
+for...of synax works for objects that are iterable like arrays and strings.
 
 const author = {
   firstName: "Zadie",
@@ -11,6 +13,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of object.values(author)) {
   console.log(value);
 }
